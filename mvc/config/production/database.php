424ed5e8a0b6a7fe424175a85eb1e785ea6da32a
erlong.duan <dragon-del@hotmail.com>
2016-10-15 23:56:09 +0900

    菜单变更

--- mvc/config/production/database.php
+++ mvc/config/production/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '123456789';
+$db['default']['password'] = 'W@rl0ve123';
 $db['default']['database'] = 'school';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
